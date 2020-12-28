3c8dc9e65d97107cadbf612a2e9ac36f15dbb2b9
william koller <wkoller25@gmail.com>
2020-12-28 12:58:30 -0300

    chore: init project

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['src/app/models/*.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+  cli: {
+    entitiesDir: 'src/database/migrations',
+  },
+}
